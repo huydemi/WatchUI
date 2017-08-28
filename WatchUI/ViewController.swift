9fd523b4dd271c18394e67ff9d9760cd330c5f44
Dang Quoc Huy <huysamdua@yahoo.com>
2017-08-28 17:15:39 +0700

    Initial Commit

---
+++ WatchUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchUI
+//
+//  Created by Dang Quoc Huy on 8/28/17.
+//  Copyright © 2017 Dang Quoc Huy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
